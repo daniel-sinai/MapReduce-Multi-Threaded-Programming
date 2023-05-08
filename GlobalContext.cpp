2bfcbc5b35516074a164b0d7bde56021596ab4dd
daniel-sinai <daniel.sinai198@gmail.com>
2023-05-08 15:57:50 +0300

    Maid work

--- GlobalContext.cpp
+++ GlobalContext.cpp
@@ -4,6 +4,9 @@ GlobalContext::GlobalContext (const MapReduceClient &client,
                               const InputVec &inputVec, OutputVec &outputVec,
                               int multiThreadLevel)
 {
+    this->input_vec = inputVec;
+    this->output_vec = outputVec;
+    this->client = &client;
   for (int i = 0; i < multiThreadLevel; ++i) {
       ThreadContext *new_context = new ThreadContext(i, &this->atomic_counter);
       contexts.push_back (new_context);
@@ -12,9 +15,22 @@ GlobalContext::GlobalContext (const MapReduceClient &client,
   for (int i = 0; i < multiThreadLevel; ++i) {
       pthread_t *new_thread = new pthread_t();
       pthread_create(new_thread, NULL, nullptr, contexts[i]);
+      threads.push_back(new_thread);
     }
 
-  for (int i = 0; i < multiThreadLevel; ++i) {
-      pthread_join(*(threads[i]), NULL);
-    }
+//  for (int i = 0; i < multiThreadLevel; ++i) {
+//      pthread_join(*(threads[i]), NULL);
+//    }
+}
+
+void GlobalContext::job_manager () {
+    // map phase till end list
+    // wait for all map phase
+    // wait for shuffle
+    // reduce phase till end list
+    // terminate
+}
+
+void GlobalContext::map_manager() {
+
 }
--- GlobalContext.h
+++ GlobalContext.h
@@ -10,11 +10,18 @@
 #ifndef _GLOBALCONTEXT_H_
 #define _GLOBALCONTEXT_H_
 
+//typedef void (*map_function) (const K1*, const V1*, void*);
+//typedef void (*reduce_function) (const IntermediateVec*, void*);
+// daniel is gay 1
+
 class GlobalContext {
  private:
   std::vector<pthread_t*> threads;
   std::vector<ThreadContext*> contexts;
   std::atomic<uint32_t> atomic_counter {0};
+  OutputVec output_vec;
+  InputVec input_vec;
+  const MapReduceClient* client;
  public:
   GlobalContext (const MapReduceClient &client, const InputVec &inputVec,
                  OutputVec &outputVec, int multiThreadLevel);
